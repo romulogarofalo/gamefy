b8b8ddb72bbbf30618055b879a3466bd649f04c1
Romulo <romulo@7bi.me>
2017-09-17 17:59:40 -0300

    done: modal to edit the class, done:organizate the files in specific folders and update of meteor

---
+++ client/layouts/Class/EditClass/EditClass.js
@@ -0,0 +1,19 @@
+Template.EditClass.events({
+    'click #cancela': function (event, instance) {
+        event.preventDefault();
+        $('#edit-class').modal('close');
+    },
+
+    'click #atualiza': function (event, template) {
+        event.preventDefault();
+        let nome = $('#nomeClasse').val();
+        let desc = $('#descricaoClasse').val();
+        let idClasse = document.querySelector('#spanId').textContent;
+        Meteor.call('classes.update', nome,desc,idClasse);
+        //template.find(".edit-class-form").reset();
+        document.querySelector('#nomeClasse').value = "";
+        document.querySelector('#descricaoClasse').value = "";
+        $('#edit-class').modal('close');
+
+    }
+});
---
+++ client/layouts/Class/NewClass/NewClass.js
@@ -0,0 +1,86 @@
+Template.ClassList.onCreated(function () {
+    const self = this;
+    self.autorun(function () {
+        self.subscribe('classes');
+    });
+    this.state = new ReactiveDict();
+    const instance = Template.instance();
+    instance.state.set('skip', 0);
+});
+
+Template.ClassList.events({
+    'click .black-text': function (event, instance) {
+        //alert('aqui');
+        var id = this._id;
+        var nome = this.name;
+        var descr = this.description;
+        console.log(id);
+        console.log(nome);
+        console.log(descr);
+        //console.log("aaaa");
+        $('#edit-class').modal('open');
+        document.querySelector('#spanId').textContent = id;
+        document.querySelector('#nomeClasse').value = nome;
+        document.querySelector('#descricaoClasse').value = descr;
+        $('#nomeClasse').focus();
+    }
+
+});
+
+
+Template.ClassList.helpers({
+
+    classes: () => {
+        const instance = Template.instance();
+        return Classes.find({}, { limit: 3, skip: instance.state.get('skip') });
+    },
+
+    hasClasses: () => {
+        return Classes.find({}).count() > 0;
+    },
+
+    addClass: () => {
+        return function () {
+            $('#new-class').modal('open');
+        }
+    },
+
+    prevClasses: () => {
+        const instance = Template.instance();
+        return function () {
+            if (instance.state.get('skip') >= 3) {
+                instance.state.set('skip', instance.state.get('skip') - 3);
+            }
+        }
+    },
+
+    nextClasses: () => {
+        const instance = Template.instance();
+        return function () {
+            if (instance.state.get('skip') + 3 < Classes.find({}).count()) {
+                instance.state.set('skip', instance.state.get('skip') + 3);
+            }
+        }
+    }
+
+});
+
+$(document).ready(function () {
+    $('.modal').modal();
+});
+
+
+Template.NewClass.events({
+    'submit .new-class-form': function (event, template) {
+
+        event.preventDefault();
+
+        // Get value from form element
+        const target = event.target;
+        const new_class = { name: target.name.value, description: target.description.value };
+        Meteor.call('classes.insert', new_class);
+        template.find(".new-class-form").reset();
+        $('#modal1').modal('close');
+
+    }
+});
